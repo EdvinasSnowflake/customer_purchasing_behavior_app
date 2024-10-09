cbf2bf7c2f0c75fcb31afb481f9aeb0f365b5b71
EdvinasSnowflake <edvmig123@gmail.com>
2024-10-09 22:15:23 +0300

    Updated snowflake connector

--- app.py
+++ app.py
@@ -9,15 +9,23 @@ st.title("Customers Purchasing Behaviour")
 # You can replace the placeholders with your Snowflake credentials
 @st.cache_resource
 def create_snowflake_connection():
-    conn = snowflake.connector.connect(
-        user=st.secrets["snowflake"]["user"],
-        password=st.secrets["snowflake"]["password"],
-        account=st.secrets["snowflake"]["account"],
-        warehouse=st.secrets["snowflake"]["warehouse"],
-        database=st.secrets["snowflake"]["database"],
-        schema=st.secrets["snowflake"]["schema"]
-    )
-    return conn
+    try:
+        conn = snowflake.connector.connect(
+            user=st.secrets["snowflake"]["user"],
+            password=st.secrets["snowflake"]["password"],
+            account=st.secrets["snowflake"]["account"],
+            role=st.secrets["snowflake"]["role"],
+            warehouse=st.secrets["snowflake"]["warehouse"],
+            database=st.secrets["snowflake"]["database"],
+            schema=st.secrets["snowflake"]["schema"]
+        )
+        return conn
+    except snowflake.connector.errors.ProgrammingError as e:
+        st.error(f"Programming error: {e}")
+    except snowflake.connector.errors.DatabaseError as e:
+        st.error(f"Database error: {e}")
+    except Exception as e:
+        st.error(f"Unexpected error: {e}") 
 
 # Function to fetch data from the Snowflake customers table
 def fetch_customers_data():
